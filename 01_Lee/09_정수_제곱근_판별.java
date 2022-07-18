7c6be3a75a68c4906eee627317984a9addea3142
lkhun9311 <96328441+lkhun9311@users.noreply.github.com>
2022-07-18 09:05:53 +0900

    Update 09_정수_제곱근_판별.java

--- 01_Lee/09_정수_제곱근_판별.java
+++ 01_Lee/09_정수_제곱근_판별.java
@@ -2,10 +2,10 @@ class Solution {
     public long solution(long n) {
         long answer = 0;
         double sqrt = Math.sqrt(n);
-        long number = 0;
+        double number = 0;
 
         if(sqrt == (int)sqrt){
-            number = (long)sqrt+1;
+            number = sqrt+1;
             answer = (long)Math.pow(number, 2);
         } else {
             answer = -1;
